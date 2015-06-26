2f6c57b4c5c0b76f78e1e0a6cd739dac813b687c
Lucas Liendo <mindmaster@gmail.com>
2015-06-26 19:32:30 -0300

    - Rewrote to be a Python script. Now is runnable in any platform.

--- run-tests.py
+++ run-tests.py
@@ -1,5 +1,29 @@
-#!/bin/bash
+#!/usr/bin/env python
 
-# TODO: Make the tests run anywhere (avoid a bash script).
+# -*- coding: utf-8 -*-
 
-nosetests -v --no-byte-compile
+"""
+This file is part of Radar.
+
+Radar is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Radar is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+Lesser GNU General Public License for more details.
+
+You should have received a copy of the Lesser GNU General Public License
+along with Radar. If not, see <http://www.gnu.org/licenses/>.
+
+Copyright 2015 Lucas Liendo.
+"""
+
+
+from nose import run as nose_run
+
+
+if __name__ == '__main__':
+    nose_run(argv=['-w', './radar/tests', '--no-byte-compile'])
