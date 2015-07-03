582797b9e840d59814b9aec8d2101b4c454dcb59
Lucas Liendo <mindmaster@gmail.com>
2015-07-03 20:35:04 -0300

    - Added setUp() method and extracted repeated code.  - Added test_monitors_are_equal(), test_monitors_are_not_equal(), test_monitor_updates_check_status() and test_monitor_does_not_update_check_monitor() tests.

--- radar/tests/test_monitor.py
+++ radar/tests/test_monitor.py
@@ -35,6 +35,15 @@ class DummyClient(Client):
 
 
 class TestMonitor(TestCase):
+    def setUp(self):
+        self.dummy_client = DummyClient(address='192.168.0.1', port=10000)
+        self.checks = [Check(name='Load average', path='load_average')]
+        self.monitor = Monitor(
+            addresses=[AddressRange('192.168.0.1 - 192.168.0.100')],
+            checks=self.checks,
+            contacts=[Contact(name='name', email='name@contact.org')]
+        )
+
     def test_monitor_does_not_get_duplicates(self):
         address = Address('192.168.0.1')
         address_range = AddressRange('192.168.0.1 - 192.168.0.100')
@@ -60,35 +69,54 @@ class TestMonitor(TestCase):
         Monitor(addresses=[Address('192.168.0.1')])
 
     def test_monitor_matches_client(self):
-        monitor = Monitor(addresses=[AddressRange('192.168.0.1 - 192.168.0.100')], checks=[Check(name='Load average', path='load_average')])
-        self.assertTrue(monitor.matches(DummyClient(address='192.168.0.1', port=10000)))
+        self.assertTrue(self.monitor.matches(self.dummy_client))
 
     def test_monitor_does_not_match_client_address(self):
-        monitor = Monitor(addresses=[AddressRange('192.168.0.1 - 192.168.0.100')], checks=[Check(name='Load average', path='load_average')])
-        self.assertFalse(monitor.matches(DummyClient(address='192.168.0.101', port=10000)))
+        self.assertFalse(self.monitor.matches(DummyClient(address='192.168.0.101', port=10000)))
 
     def test_monitor_does_not_match_client_because_is_already_connected(self):
-        dummy_client = DummyClient(address='192.168.0.1', port=10000)
-        monitor = Monitor(addresses=[AddressRange('192.168.0.1 - 192.168.0.100')], checks=[Check(name='Load average', path='load_average')])
-        monitor.add_client(dummy_client)
-        self.assertFalse(monitor.matches(dummy_client))
+        self.monitor.add_client(self.dummy_client)
+        self.assertFalse(self.monitor.matches(self.dummy_client))
 
     def test_add_client_succeeds(self):
-        monitor = Monitor(addresses=[AddressRange('192.168.0.1 - 192.168.0.100')], checks=[Check(name='Load average', path='load_average')])
-        self.assertTrue(monitor.add_client(DummyClient(address='192.168.0.1', port=10000)))
+        self.assertTrue(self.monitor.add_client(self.dummy_client))
 
     def test_add_client_fails(self):
-        monitor = Monitor(addresses=[AddressRange('192.168.0.1 - 192.168.0.100')], checks=[Check(name='Load average', path='load_average')])
-        self.assertFalse(monitor.add_client(DummyClient(address='192.168.0.101', port=10000)))
+        self.assertFalse(self.monitor.add_client(DummyClient(address='192.168.0.101', port=10000)))
 
     def test_remove_client(self):
-        dummy_client = DummyClient(address='192.168.0.1', port=10000)
-        monitor = Monitor(addresses=[AddressRange('192.168.0.1 - 192.168.0.100')], checks=[Check(name='Load average', path='load_average')])
-        monitor.add_client(dummy_client)
-        self.assertEqual(len(monitor.active_clients), 1)
-        self.assertTrue(monitor.remove_client(dummy_client))
+        self.monitor.add_client(self.dummy_client)
+        self.assertEqual(len(self.monitor.active_clients), 1)
+        self.assertTrue(self.monitor.remove_client(self.dummy_client))
+
+    def test_monitors_are_equal(self):
+        another_monitor = Monitor(
+            addresses=[AddressRange('192.168.0.1 - 192.168.0.100')],
+            checks=[Check(name='Load average', path='load_average')],
+            contacts=[Contact(name='name', email='name@contact.org')]
+        )
+        self.assertEqual(self.monitor, another_monitor)
+
+    def test_monitors_are_not_equal(self):
+        another_monitor = Monitor(
+            addresses=[AddressRange('192.168.0.0 - 192.168.0.100')],
+            checks=[Check(name='Load average', path='load_average')]
+        )
+        self.assertNotEqual(self.monitor, another_monitor)
 
     def test_monitors_set(self):
-        monitor = Monitor(addresses=[AddressRange('192.168.0.1 - 192.168.0.100')], checks=[Check(name='Load average', path='load_average')])
-        duplicated_monitors = [monitor, monitor]
-        self.assertTrue(len(set(duplicated_monitors)), 1)
+        self.assertTrue(len(set([self.monitor, self.monitor])), 1)
+
+    def test_monitor_updates_check_status(self):
+        check_status = {'status': Check.STATUS['ERROR'], 'id': self.checks[0].id}
+        self.monitor.add_client(self.dummy_client)
+        updated_checks = self.monitor.update_checks(self.dummy_client, [check_status])
+        self.assertEqual(list(self.monitor.active_clients[0]['checks']).pop().status, Check.STATUS['ERROR'])
+        self.assertNotEqual(updated_checks, {})
+
+    def test_monitor_does_not_update_check_status(self):
+        check_status = {'status': Check.STATUS['ERROR'], 'id': self.checks[0].id + 1}
+        self.monitor.add_client(self.dummy_client)
+        updated_checks = self.monitor.update_checks(self.dummy_client, [check_status])
+        self.assertEqual(list(self.monitor.active_clients[0]['checks']).pop().status, Check.STATUS['UNKNOWN'])
+        self.assertEqual(updated_checks, {})
