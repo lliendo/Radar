ff7659dc988a1277c734743d01aea7bdc1a50f95
Lucas Liendo <mindmaster@gmail.com>
2015-10-14 01:08:14 -0300

    - Updated setup.py.

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     version='0.0.1a',
     packages=find_packages(exclude=['docs', 'tests', 'scripts']),
     author='Lucas Liendo',
-    author_email='mindmaster@gmail.com',
+    author_email='liendolucas84@gmail.com',
     keywords='monitor monitoring system administration',
     license='LGPLv3',
     zip_safe=False,
