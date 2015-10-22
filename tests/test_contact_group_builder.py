7d4f81fa3661d94455428984f964dae1db1dbcd6
Lucas Liendo <mindmaster@gmail.com>
2015-10-22 17:48:14 -0300

    - We now use mocks.

--- tests/test_contact_group_builder.py
+++ tests/test_contact_group_builder.py
@@ -22,21 +22,16 @@ Copyright 2015 Lucas Liendo.
 
 from unittest import TestCase
 from nose.tools import raises
-from StringIO import StringIO
-from yaml import safe_load
+from yaml import load
+from mock import patch
 from radar.config import ConfigError
 from radar.config.server import ContactGroupBuilder
 from radar.contact import Contact
 
 
-class MockedContactGroupBuilder(ContactGroupBuilder):
-    def _read_config(self, path):
-        return safe_load(path)
-
-
 class TestContactBuilder(TestCase):
     def test_repeated_contacts_group_are_excluded(self):
-        f = StringIO("""
+        input_yaml = """
         - contact group:
             name: A
             contacts:
@@ -58,21 +53,22 @@ class TestContactBuilder(TestCase):
                 - contact:
                     name: C
                     email: C
-        """)
+        """
 
         contacts = [
             Contact(name='B', email='B'),
             Contact(name='C', email='C'),
         ]
 
-        contact_group = MockedContactGroupBuilder(f).build(contacts)
-        self.assertEqual(len(contact_group), 1)
-        self.assertTrue(contacts[0] in contact_group[0].contacts)
-        self.assertTrue(contacts[1] in contact_group[0].contacts)
+        with patch.object(ContactGroupBuilder, '_read_config', return_value=load(input_yaml)):
+            contact_group = ContactGroupBuilder(None).build(contacts)
+            self.assertEqual(len(contact_group), 1)
+            self.assertTrue(contacts[0] in contact_group[0].contacts)
+            self.assertTrue(contacts[1] in contact_group[0].contacts)
 
     @raises(ConfigError)
     def test_non_existent_contact_raises_config_error(self):
-        f = StringIO("""
+        input_yaml = """
         - contact group:
             name: A
             contacts:
@@ -82,16 +78,17 @@ class TestContactBuilder(TestCase):
 
                 - contact:
                     name: C
-        """)
+        """
 
         contacts = [
             Contact(name='B', email='B'),
         ]
 
-        MockedContactGroupBuilder(f).build(contacts)
+        with patch.object(ContactGroupBuilder, '_read_config', return_value=load(input_yaml)):
+            ContactGroupBuilder(None).build(contacts)
 
     def test_contacts_are_referenced(self):
-        f = StringIO("""
+        input_yaml = """
         - contact group:
             name: A
             contacts:
@@ -100,14 +97,15 @@ class TestContactBuilder(TestCase):
 
                 - contact:
                     name: C
-        """)
+        """
 
         contacts = [
             Contact(name='B', email='B'),
             Contact(name='C', email='C'),
         ]
 
-        contact_group = MockedContactGroupBuilder(f).build(contacts)
-        self.assertEqual(len(contact_group[0].contacts), 2)
-        self.assertTrue(contacts[0] in contact_group[0].contacts)
-        self.assertTrue(contacts[1] in contact_group[0].contacts)
+        with patch.object(ContactGroupBuilder, '_read_config', return_value=load(input_yaml)):
+            contact_group = ContactGroupBuilder(None).build(contacts)
+            self.assertEqual(len(contact_group[0].contacts), 2)
+            self.assertTrue(contacts[0] in contact_group[0].contacts)
+            self.assertTrue(contacts[1] in contact_group[0].contacts)
